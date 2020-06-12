3c4337d7733556056cc7ce57c52f1814199609c6
CzupakAdrian <62177095+CzupakAdrian@users.noreply.github.com>
2020-06-12 18:14:36 +0200

    Init repos

---
+++ Smart-Home/build/CentralUnit/CMakeFiles/CentralUnit.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "D:/00coding/repositories/Smart-Home/CentralUnit/source/main.cpp" "D:/00coding/repositories/Smart-Home/build/CentralUnit/CMakeFiles/CentralUnit.dir/source/main.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "CentralUnit"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Smart-Home/build/CentralUnit/CMakeFiles/CentralUnit.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CentralUnit.dir/source/main.cpp.obj"
+  "CentralUnit.exe"
+  "CentralUnit.exe.manifest"
+  "CentralUnit.pdb"
+  "libCentralUnit.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CentralUnit.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
